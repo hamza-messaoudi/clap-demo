6d31ef52961b0b802817ba88b4733b37a271abd5
Hamza Messaoudi <messaoudi.hamza216@gmail.com>
2023-06-19 16:42:40 +0200

    Subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,63 @@
-use clap::{Args, Parser};
+use clap::{Args, Parser, Subcommand};
 
-fn main() {
-    let args = Arguments::parse();
-    println!("{:?}", args);
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
 }
 
-#[derive(Parser,Debug)]
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds files to myapp
+    Add(Arguments),
+
+    /// Removes files from myapp
+    Remove(Arguments),
+}
+
+#[derive(Parser, Debug, Clone)]
 struct Arguments {
     // mandatory args (must have higher indexes)
     #[arg(short = 'm')]
     mandatory_arg: usize,
 
-    //exclusive arg group
+    // exclusive arg group
     #[command(flatten)]
     exclusive_arg_group: ExclusiveArgGroup,
 
     // optional args
     #[arg(short = 'o')]
     optional_arg: Option<usize>,
-
 }
 
-#[derive(Args,Debug)]
+#[derive(Args, Debug, Clone)]
 #[group(required = true, multiple = false)]
-struct ExclusiveArgGroup{
+struct ExclusiveArgGroup {
+    // a XOR b
     #[arg(short = 'a')]
-    arg_a : bool,
+    arg_a: bool,
 
     #[arg(short = 'b')]
-    arg_b : bool,
-
+    arg_b: bool,
 }
 
+fn main() {
+    let cli = Cli::parse();
 
-// arg relations tutorial 
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Add(args) => {
+            println!("{:?}", args);
+        }
+
+        Commands::Remove(args) => {
+            println!("{:?}", args);
+        }
+    }
+}
+
+// arg relations tutorial
 // https://docs.rs/clap/latest/clap/_derive/_tutorial/index.html#argument-relations
